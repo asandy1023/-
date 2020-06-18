38e67cb80b3b794f4abff4a525c0f55736a4f6db
asandy1023 <asandy1023@gmail.com>
2020-06-18 22:55:57 +0800

    10

--- js.js
+++ js.js
@@ -69,7 +69,7 @@ function clickspan(f) {
   });
   //f.close();
 }
-var a = [],c=10, tt = 0;
+var a = [0],c=10, tt = 0;
 var b = [0];
 
 
@@ -100,7 +100,7 @@ function updata(y) {
         for (var u = 1; u < a.length; u++) {
           
           if (a[u] != 0) {
-            var tblRow = '<p style=" padding-bottom:40px;">' + d[u] + "數量:" + c + '</p>'
+            var tblRow = '<p style=" padding-bottom:40px;">' + d[u] + "數量:" + a[u] + '</p>'
             c++;
             $(tblRow).appendTo("#item");
           }
